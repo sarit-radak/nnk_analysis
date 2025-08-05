6bed0e296680def914a0d7417b7b7431ca223a3f
sarit-radak <148570255+sarit-radak@users.noreply.github.com>
2025-08-05 12:14:21 -0700

    Delete 4-calc_fc.sh

--- 4-calc_fc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=calc_fc
-#SBATCH --time=24:00:00
-#SBATCH --output=logs/calc_fc.out
-#SBATCH --error=logs/calc_fc.err
-
-# convert counts to frequencies
-find 5_nnk_count -maxdepth 1 -type f -name '*.xlsx' | parallel -j 16 'python3 -u pythonfiles/7-convert_to_freq.py'
